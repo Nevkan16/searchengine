2a2e4a7f5bc03a3fdf7b327f603ab1f4382cae8a
vladislav_eroshkin <stiff_j@mai.ru>
2024-12-15 18:05:42 +0300

    added Reset autoIncrement

--- src/main/java/searchengine/services/DataService.java
+++ src/main/java/searchengine/services/DataService.java
@@ -1,5 +1,6 @@
 package searchengine.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.config.Site;
@@ -8,51 +9,57 @@ import searchengine.model.SiteEntity;
 import searchengine.repository.PageRepository;
 import searchengine.repository.SiteRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Service
 public class DataService {
 
-    private final SiteRepository siteRepository;
-    private final PageRepository pageRepository;
-    private final SitesList sitesList;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    public DataService(SiteRepository siteRepository, PageRepository pageRepository, SitesList sitesList) {
-        this.siteRepository = siteRepository;
-        this.pageRepository = pageRepository;
-        this.sitesList = sitesList;
-    }
+    @Autowired // автоматическое внедрение зависимостей
+    private SiteRepository siteRepository;
 
+    @Autowired
+    private PageRepository pageRepository;
+
+    @Autowired
+    private SitesList sitesList;
+
+    @Transactional
     public void deleteSiteData() {
-        for (Site list : sitesList.getSites()) {
-            System.out.println(list.getUrl());
-        }
         for (Site siteConfig : sitesList.getSites()) {
             System.out.println("Проверяем сайт: " + siteConfig.getUrl());
 
-            // Проверяем, существует ли сайт в базе данных
             SiteEntity siteEntity = siteRepository.findByUrl(siteConfig.getUrl());
             if (siteEntity != null) {
                 System.out.println("Найден сайт в БД: " + siteConfig.getUrl() + ". Удаляем его.");
 
                 try {
-                    // Удаляем сам сайт
-                    siteRepository.delete(siteEntity);
-                    System.out.println("Сайт удален: " + siteConfig.getUrl());
-                } catch (Exception e) {
-                    System.out.println("Ошибка с удалением сайта: " + siteConfig.getUrl());
-                    continue;
-                }
-
-                try {
-                    // Удаляем страницы, связанные с этим сайтом
                     pageRepository.deleteBySite(siteEntity);
                     System.out.println("Страницы удалены для сайта: " + siteConfig.getUrl());
+
+                    siteRepository.delete(siteEntity);
+                    System.out.println("Сайт удален: " + siteConfig.getUrl());
+
+                    resetAutoIncrement("page");
+                    resetAutoIncrement("site");
                 } catch (Exception e) {
-                    System.out.println("Ошибка с удалением страниц для сайта: " + siteConfig.getUrl());
+                    System.out.println("Ошибка при удалении сайта или страниц: " + e.getMessage());
                 }
             } else {
                 System.out.println("Сайт не найден в БД: " + siteConfig.getUrl());
             }
         }
     }
-}
 
+    private void resetAutoIncrement(String tableName) {
+        try {
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
+            System.out.println("Автоинкремент сброшен для таблицы: " + tableName);
+        } catch (Exception e) {
+            System.out.println("Ошибка при сбросе автоинкремента для таблицы " + tableName + ": " + e.getMessage());
+        }
+    }
+}
