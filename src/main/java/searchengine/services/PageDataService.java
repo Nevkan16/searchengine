0a0248ce696dde5f676276f6cc1a60cf7800bc48
vladislav_eroshkin <stiff_j@mai.ru>
2024-12-18 15:31:58 +0300

    added checking for duplicated page

--- src/main/java/searchengine/services/PageDataService.java
+++ src/main/java/searchengine/services/PageDataService.java
@@ -1,13 +1,14 @@
 package searchengine.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.PageEntity;
 import searchengine.model.SiteEntity;
 import searchengine.repository.PageRepository;
 import searchengine.repository.SiteRepository;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PageDataService {
@@ -15,14 +16,23 @@ public class PageDataService {
     private final SiteRepository siteRepository;
     @Transactional
     public void addPage(SiteEntity site, String path, int code, String content) {
-        PageEntity pageEntity = new PageEntity();
-        pageEntity.setSite(site);
-        pageEntity.setPath(path);
-        pageEntity.setCode(code);
-        pageEntity.setContent(content);
+        try {
 
-        // Сохраняем страницу в базе данных
-        pageRepository.save(pageEntity);
+            PageEntity pageEntity = new PageEntity();
+            pageEntity.setSite(site);
+            pageEntity.setPath(path);
+            pageEntity.setCode(code);
+            pageEntity.setContent(content);
+
+            if (!pageRepository.existsByPath(path)) {
+                // Сохраняем страницу в базе данных
+                pageRepository.save(pageEntity);
+            } else {
+                log.info("Страница уже существует");
+            }
+        } catch (Exception e) {
+            log.info("Ошибка при сохранении страницы");
+        }
     }
 
     // Метод для получения URL сайта по ID
